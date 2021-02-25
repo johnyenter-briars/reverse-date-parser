4e207092ca9569a529a151aecefd9c1fd7fd6eb4
John Yenter-Briars <jyenterbriars@gmail.com>
2021-02-25 08:34:08 -0600

    basic parser working. hooked in test suite. not all tests passing

--- parser.ts
+++ parser.ts
@@ -22,34 +22,64 @@ function get_dd(dateString: string): Array<String>{
     return null;
 }
 
+// Matches date elements like '2021', '0001', '9999'
 function get_yyyy(dateString: string){
-    return [];
+    var regexp = new RegExp('^[0-9]{4}$');
+
+    if(regexp.test(dateString)){
+        return regexp.exec(dateString);
+    }
+    return null;
+}
+
+// Matches date elements like '01', '08', '12'
+function get_MM(dateString: string){
+    if(dateString.length != 2) return null;
+
+    var regexp = new RegExp('^[0-2][1-9]$'),
+        regexp2 = new RegExp('^[1-9][3-9]$');
+
+    if(regexp.test(dateString) && !regexp2.test(dateString)){
+        return regexp.exec(dateString);
+    }
+    return null;
 }
 
 
 let possibleSections:{  sectionName: string, 
                         formatSpecifier: string, 
                         regexFunc: Function,
-                        currentValue: string }[] 
+                        currentValue: string,
+                        type: string }[] 
 = [
     {
         sectionName: "day_of_month",
         formatSpecifier: "d",
         regexFunc: get_d,
-        currentValue: null
+        currentValue: null,
+        type: "day"
     },
     {
         sectionName: "day_of_month_two_digits",
         formatSpecifier: "dd",
         regexFunc: get_dd,
-        currentValue: null
+        currentValue: null,
+        type: "day"
     },
-    // {
-    //     sectionName: "day_of_month_two_digits",
-    //     formatSpecifier: "dd",
-    //     regexFunc: get_dd,
-    //     currentValue: null
-    // }
+    {
+        sectionName: "month_two_digits",
+        formatSpecifier: "MM",
+        regexFunc: get_MM,
+        currentValue: null,
+        type: "month"
+    },
+    {
+        sectionName: "year_four_digits",
+        formatSpecifier: "yyyy",
+        regexFunc: get_yyyy,
+        currentValue: null,
+        type: "year"
+    }
 ]
 
 let possibleSplits:{    splitName: string,
@@ -74,21 +104,6 @@ let possibleSplits:{    splitName: string,
     }
 ]
 
-
-
-
-// const fourDigitYear = /\d{4}/g;
-
-
-// function findFourDigitYear(dateString) {
-//     var result = dateString.match(fourDigitYear);
-
-//     if(result && result.length == 1){
-//         return result[0];
-//     }
-//     return false;
-// }
-
 // Returns an array of possible character codes the given element string COULD represent
 function matchElement(element: string): Array<string>{
     var possibleMatches = new Array();
@@ -104,9 +119,27 @@ function matchElement(element: string): Array<string>{
     return possibleMatches;
 }
 
+// Checks that there isn't a duplicate type - for example 'dd-dd-yyyy' is invalid
+function validMatch(matchElement: any): boolean{
+    return matchElement.possibleMatch0.type != matchElement.possibleMatch1.type &&
+            matchElement.possibleMatch0.type != matchElement.possibleMatch2.type &&
+            matchElement.possibleMatch1.type != matchElement.possibleMatch2.type;
+}   
 
 
-function parseDateString(dateString: string): string {
+function removeInvalidMatchElements(possibleMatches: any){
+    var sanitizedMatches = [];
+
+    for(let matchPossibility of possibleMatches){
+        if(validMatch(matchPossibility)){
+            sanitizedMatches.push(matchPossibility);
+        }
+    }
+
+    return sanitizedMatches;
+}
+
+function parseDateString(dateString: string): Array<string> {
     // Step 1: split the string into groups
     for(let possibleSplit of possibleSplits){
         possibleSplit.splitStringArray = dateString.split(possibleSplit.splitOn);
@@ -115,6 +148,9 @@ function parseDateString(dateString: string): string {
     let finalSplit = possibleSplits.find((obj) => obj.splitStringArray.length == 3);
 
     let dateElements = finalSplit.splitStringArray;
+
+    // Needs to be date nicly split into three sections
+    if(dateElements.length != 3) return null;
     
     let elementMatches = [];
     // Step 2: try to match the elements of the split into their closest match
@@ -124,80 +160,78 @@ function parseDateString(dateString: string): string {
         elementMatches.push({element, possibleMatches})
     }
 
-    // console.log(elementMatches);
+    // There needs to be at least one match per each date element
+    if(elementMatches.some((ele) => ele.possibleMatches.length < 1)) return null;
 
-    console.log(elementMatches[2].possibleMatches)
+    var possibleStringFormats = new Array<string>();
 
-    
+    // Step 3: Iterate through all possible match configurations
+    for(let possibleMatch0 of elementMatches[0].possibleMatches){
+        for(let possibleMatch1 of elementMatches[1].possibleMatches){
+            for(let possibleMatch2 of elementMatches[2].possibleMatches){
+                // Only if the match configuration is valid, we can append the string
+                if(validMatch({possibleMatch0, possibleMatch1, possibleMatch2})){
+                    possibleStringFormats.push(`${possibleMatch0.formatSpecifier}${finalSplit.splitOn}${possibleMatch1.formatSpecifier}${finalSplit.splitOn}${possibleMatch2.formatSpecifier}`)
+                }        
+            }
+        }
+    }
 
-
-    // for(section of possibleSections){
-    //     console.log(section.regexFunc(dateString))
-    // }
-    
-    return;
+    return possibleStringFormats;
 }
 
-parseDateString("2021-02-01");
-
-// console.log(get_d("p9403"));
-// console.log(get_d("01"));
-// console.log(get_d("08"));
-// console.log(get_d("1"));
-// console.log(get_d("11"));
-// console.log(get_d("25"));
-// console.log(get_d("32"));
-// console.log(get_d("37"));
-// console.log(get_d("58"));
-// console.log(get_dd("01"));
-// console.log(get_dd("08"));
-// console.log(get_dd("1"));
-// console.log(get_dd("9"));
-// console.log(get_dd("11"));
-// console.log(get_dd("25"));
-// console.log(get_dd("31"));
-// console.log(get_dd("32"));
-// console.log(get_dd("37"));
-// console.log(get_dd("58"));
-
-
-
+// Testing suite
 let testSamples = [
     {
         data: "2021-2-1",
         isValid: true,
-        result: "yyyy-M-d"
+        result: ["yyyy-M-d", "yyyy-d-M"]
 
     },
     {
         data: "2021-02-01",
         isValid: true,
-        result: "yyyy-MM-dd"
+        result: ["yyyy-MM-dd", "yyyy-dd-MM"]
 
     },
     {
         data: "July 31st, 2021",
         isValid: true,
-        result: true
+        result: ["MMM ddst, yyyy"]
 
     },
     {
         data: "6/12/2021",
         isValid: true,
-        result: true
+        result: ["m/d/2021", "m/dd/2021"]
 
     },
     {
         data: "06-06-2021",
         isValid: true,
-        result: true
+        result: ["dd-mm-yyyy", "mm-dd-yyyy"]
 
     },
     {
         data: "06/12/98",
         isValid: true,
-        result: true
+        result: ["mm/dd/yy", "dd/mm/yy"]
 
     }
 ]
+
+for(let sample of testSamples){
+    let result = parseDateString(sample.data);
+
+    if(result != null &&
+        result.length === sample.result.length &&
+        result.every((str) => result.includes(str))
+        ){
+        console.log(`Pass for ${sample.data}`);
+    }
+    else{
+        console.log(`Fail for ${sample.data}`);
+        console.log(`${result} does not match ${sample.result}`);
+    }
+}
 
