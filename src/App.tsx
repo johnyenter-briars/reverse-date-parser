4d333b7bee35a1cbda5796aed5c5d91dd32947e8
John Yenter-Briars <jyenterbriars@gmail.com>
2021-10-17 14:00:41 -0500

    add initial front end

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,92 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { parseDateString } from './parser/Parser';
+import { Avatar, FormControl, IconButton, InputLabel, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, MenuItem, Select, TextField } from '@mui/material';
+import { Box } from '@mui/system';
 
-function App() {
-  let result = parseDateString("2021-2-1", "C#")
+const App: React.FC = () => {
+  const [parsedOptions, setParsedOptions] = React.useState(parseDateString("2021-12-01", "C#"));
+  const [currentLanguage, setCurrentLangugae] = React.useState("C#");
+
+  const onUserInput = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
+    setParsedOptions(parseDateString(event.target.value, "C#"));
+  }
+
+  const onLanguageSelect = (event: { target: { value: React.SetStateAction<string>; }; }): void => {
+    setCurrentLangugae(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ width: '100%' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            bgcolor: 'red',
+            justifyContent: 'center',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-      <body>
-        <h1>{result}</h1>
-      </body>
+          <Box
+            sx={{
+              display: 'grid',
+              justifyContent: 'center',
+              p: 1,
+              m: 1,
+              bgcolor: 'green',
+              alignContent: 'flex-start',
+            }}
+          >
+            <Box
+              sx={{
+                bgcolor: 'blue',
+              }}
+            >
+              <TextField onChange={onUserInput} id="outlined-basic" label={currentLanguage} variant="outlined" />
+            </Box>
+            <Box
+              sx={{
+                bgcolor: 'purple',
+              }}
+            >
+              <List dense={true}>
+                {
+                  parsedOptions.map((option: string) => {
+                    return (<ListItem
+                      secondaryAction={
+                        <IconButton edge="end" aria-label="delete">
+                        </IconButton>
+                      }
+                    >
+                      <ListItemAvatar>
+                        <Avatar>
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={option}
+                        secondary={true ? 'Secondary text' : null}
+                      />
+                    </ListItem>);
+                  })
+                }
+              </List>
+            </Box>
+          </Box>
+          <Box>
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="language-input-label">Language</InputLabel>
+              <Select
+                labelId="select"
+                id="select"
+                value={currentLanguage}
+                onChange={onLanguageSelect}
+              >
+                <MenuItem value={"C#"}>C#</MenuItem>
+                <MenuItem value={"Python"}>Python</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Box>
+      </div>
     </div>
   );
 }
--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -6,33 +6,33 @@ import { MatchElement } from "./MatchElement";
 import { PossibleGrammars } from "./PossibleGrammars";
 
 
-const possibleSplits: Split[] 
-= [
-    {
-        splitName: "dash",
-        splitOn: "-",
-        splitStringArray: null
-    },
-    {
-        splitName: "foward_slash",
-        splitOn: "/",
-        splitStringArray: null
-    },
-    {
-        splitName: "space",
-        splitOn: " ",
-        splitStringArray: null
-    }
-]
+const possibleSplits: Split[]
+    = [
+        {
+            splitName: "dash",
+            splitOn: "-",
+            splitStringArray: null
+        },
+        {
+            splitName: "foward_slash",
+            splitOn: "/",
+            splitStringArray: null
+        },
+        {
+            splitName: "space",
+            splitOn: " ",
+            splitStringArray: null
+        }
+    ]
 
 // Returns an array of possible character codes the given element string COULD represent
-function matchElement(element: string, possibleElements: Array<PossibleElement> ): Array<PossibleElement>{
+function matchElement(element: string, possibleElements: Array<PossibleElement>): Array<PossibleElement> {
     var possibleMatches = new Array<PossibleElement>();
-    
-    for(let possibleSection of possibleElements){
+
+    for (let possibleSection of possibleElements) {
         var result = possibleSection.regexFunc(element)
-        
-        if(result){
+
+        if (result) {
             possibleMatches.push(possibleSection);
         }
     }
@@ -40,9 +40,9 @@ function matchElement(element: string, possibleElements: Array<PossibleElement> 
     return possibleMatches;
 }
 
-function findGrammar(grammarChoice: string ): Array<PossibleElement>{
-    for(let grammar of PossibleGrammars){
-        if(grammar.grammarName == grammarChoice){
+function findGrammar(grammarChoice: string): Array<PossibleElement> {
+    for (let grammar of PossibleGrammars) {
+        if (grammar.grammarName === grammarChoice) {
             return grammar.possibleElements
         }
     }
@@ -50,54 +50,54 @@ function findGrammar(grammarChoice: string ): Array<PossibleElement>{
 }
 
 // Checks that there isn't a duplicate type - for example 'dd-dd-yyyy' is invalid
-function validMatch(matchElement: MatchElement): boolean{
-    return matchElement.possibleMatch0.type != matchElement.possibleMatch1.type &&
-            matchElement.possibleMatch0.type != matchElement.possibleMatch2.type &&
-            matchElement.possibleMatch1.type != matchElement.possibleMatch2.type;
-}   
+function validMatch(matchElement: MatchElement): boolean {
+    return matchElement.possibleMatch0.type !== matchElement.possibleMatch1.type &&
+        matchElement.possibleMatch0.type !== matchElement.possibleMatch2.type &&
+        matchElement.possibleMatch1.type !== matchElement.possibleMatch2.type;
+}
 
-export function parseDateString(dateString: string, grammarChoice: string): string[] | null {
+export function parseDateString(dateString: string, languageChoice: string): string[] {
     // Step 1: split the string into groups
-    for(let possibleSplit of possibleSplits){
+    for (let possibleSplit of possibleSplits) {
         possibleSplit.splitStringArray = dateString.split(possibleSplit.splitOn);
     }
 
-    let finalSplit = possibleSplits.find((obj) => obj.splitStringArray?.length == 3);
+    let finalSplit = possibleSplits.find((obj) => obj.splitStringArray?.length === 3);
 
-    if (finalSplit == undefined) throw new Error("Unable to find appropiate split type!");
+    if (finalSplit === undefined) return new Array<string>();
 
     let dateElements = finalSplit?.splitStringArray;
 
     // Needs to be date nicly split into three sections
-    if(dateElements?.length != 3) return null;
+    if (dateElements?.length !== 3) return new Array<string>();
 
-    let possibleElements = findGrammar(grammarChoice)
-    
+    let possibleElements = findGrammar(languageChoice)
+
     let elementMatches = Array<{
         element: string,
         possibleMatches: PossibleElement[]
     }>();
 
     // Step 2: try to match the elements of the split into their closest match
-    for(let element of dateElements){
+    for (let element of dateElements!) {
         let possibleMatches = matchElement(element, possibleElements);
 
-        elementMatches.push({element, possibleMatches})
+        elementMatches.push({ element, possibleMatches })
     }
 
     // There needs to be at least one match per each date element
-    if(elementMatches.some((ele) => ele.possibleMatches.length < 1)) return null;
+    if (elementMatches.some((ele) => ele.possibleMatches.length < 1)) new Array<string>();
 
     var possibleStringFormats = new Array<string>();
 
     // Step 3: Iterate through all possible match configurations
-    for(let possibleMatch0 of elementMatches[0].possibleMatches){
-        for(let possibleMatch1 of elementMatches[1].possibleMatches){
-            for(let possibleMatch2 of elementMatches[2].possibleMatches){
+    for (let possibleMatch0 of elementMatches[0].possibleMatches) {
+        for (let possibleMatch1 of elementMatches[1].possibleMatches) {
+            for (let possibleMatch2 of elementMatches[2].possibleMatches) {
                 // Only if the match configuration is valid, we can append the string
-                if(validMatch({possibleMatch0, possibleMatch1, possibleMatch2})){
+                if (validMatch({ possibleMatch0, possibleMatch1, possibleMatch2 })) {
                     possibleStringFormats.push(`${possibleMatch0.formatSpecifier}${finalSplit.splitOn}${possibleMatch1.formatSpecifier}${finalSplit.splitOn}${possibleMatch2.formatSpecifier}`)
-                }        
+                }
             }
         }
     }
@@ -110,80 +110,84 @@ export function parseDateString(dateString: string, grammarChoice: string): stri
     Testing Suite
 */
 const testSamples = [
-    {
-        data: "2021-2-1",
-        isValid: true,
-        result: ["yyyy-M-d", "yyyy-d-M"]
+    // {
+    //     data: "2021-2-1",
+    //     isValid: true,
+    //     result: ["yyyy-M-d", "yyyy-d-M"]
 
-    },
-    {
-        data: "2021-02-01",
-        isValid: true,
-        result: ["yyyy-MM-dd", "yyyy-dd-MM"]
+    // },
+    // {
+    //     data: "2021-02-01",
+    //     isValid: true,
+    //     result: ["yyyy-MM-dd", "yyyy-dd-MM"]
 
-    },
-    {
-        data: "feb 28 0006",
-        isValid: true,
-        result: ["MMM dd yyyy", "MMM d yyyy"]
+    // },
+    // {
+    //     data: "feb 28 0006",
+    //     isValid: true,
+    //     result: ["MMM dd yyyy", "MMM d yyyy"]
 
-    },
-    {
-        data: "6/12/2021",
-        isValid: true,
-        result: ["M/d/2021", "M/dd/2021", "M/d/yyyy","M/dd/yyyy"]
+    // },
+    // {
+    //     data: "6/12/2021",
+    //     isValid: true,
+    //     result: ["M/d/2021", "M/dd/2021", "M/d/yyyy", "M/dd/yyyy"]
 
-    },
-    {
-        data: "06-06-2021",
-        isValid: true,
-        result: ["dd-MM-yyyy", "MM-dd-yyyy"]
+    // },
+    // {
+    //     data: "06-06-2021",
+    //     isValid: true,
+    //     result: ["dd-MM-yyyy", "MM-dd-yyyy"]
 
-    },
-    {
-        data: "06/12/98",
-        isValid: true,
-        result: ["dd/M/y","dd/M/yy","dd/MM/y","dd/MM/yy","MM/d/y","MM/d/yy","MM/dd/y","MM/dd/yy"]
+    // },
+    // {
+    //     data: "06/12/98",
+    //     isValid: true,
+    //     result: ["dd/M/y", "dd/M/yy", "dd/MM/y", "dd/MM/yy", "MM/d/y", "MM/d/yy", "MM/dd/y", "MM/dd/yy"]
 
-    },
-    {
-        data: "6 01 1998",
-        isValid: true,
-        result: ["M dd yyyy", "d MM yyyy"]
+    // },
+    // {
+    //     data: "6 01 1998",
+    //     isValid: true,
+    //     result: ["M dd yyyy", "d MM yyyy"]
 
-    },
-    {
-        data: "march 1 98",
-        isValid: true,
-        result: ["MMMM d yy", "MMMM d y"]
+    // },
+    // {
+    //     data: "march 1 98",
+    //     isValid: true,
+    //     result: ["MMMM d yy", "MMMM d y"]
 
-    },
-    {
-        data: "06 sunday 1998",
-        isValid: true,
-        result: ["dd dddd yyyy"]
+    // },
+    // {
+    //     data: "06 sunday 1998",
+    //     isValid: true,
+    //     result: ["dd dddd yyyy"]
 
-    },
+    // },
+    // {
+    //     data: "31 Fri 2",
+    //     isValid: true,
+    //     result: ["d mmm y", "dd mmm y"]
+
+    // },
     {
-        data: "31 Fri 2",
-        isValid: true,
-        result: ["d mmm y","dd mmm y"]
+        data: "this is a test",
+        isValid: false,
+        result: []
 
     }
 ]
 
 
 // console.log(parseDateString('6/12/2021'));
-for(let sample of testSamples){
+for (let sample of testSamples) {
     let result = parseDateString(sample.data, "C#");
 
-    if( result != null &&
-        result.length === sample.result.length &&
-        result.every((str) => result?.includes(str))
-        ){
+    if (result.length === sample.result.length &&
+        result.every((str) => result?.includes(str))) {
         console.log(`Pass for ${sample.data}`);
     }
-    else{
+    else {
         console.log(`Fail for ${sample.data}`);
         console.log(`${result} does not match ${sample.result}`);
     }
