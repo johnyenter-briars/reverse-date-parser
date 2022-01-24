d3d1116a8eeb3f9271498c43b7556322e55ad2be
John Yenter-Briars <jyenterbriars@gmail.com>
2022-01-23 22:29:08 -0600

    start component move

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,19 @@ import { ThemeProvider } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
-  typography: {
-    h1: {
-      fontFamily: '"Courier New", Courier, monospace',
-      fontSize: '50px'
+    typography: {
+        h1: {
+            fontFamily: '"Courier New", Courier, monospace',
+            fontSize: '50px'
+        }
     }
-  }
 })
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </ThemeProvider>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    </ThemeProvider>,
+    document.getElementById('root')
 );
