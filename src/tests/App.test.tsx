2c0fb4479bb4ca77bebba20a8689035d3fd2e823
John Yenter-Briars <jyenterbriars@gmail.com>
2022-01-29 16:30:50 -0600

    fix tests + update readme

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,177 +2,138 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { parseDateString } from '../parser/Parser';
 
-//TODO - fix this
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-const cSharpTestCases = [
-  {
-    data: "2021-2-1",
-    isValid: true,
-    result: ["yyyy-d-M", "yyyy-M-d"]
-  },
-  {
-    data: "2021-02-01",
-    isValid: true,
-    result: ["yyyy-dd-MM", "yyyy-MM-dd"]
-  },
-  {
-    data: "feb 28 0006",
-    isValid: true,
-    result: ["MMM d yyyy", "MMM dd yyyy"]
-  },
-  {
-    data: "6/12/2021",
-    isValid: true,
-    result: ["M/d/yyyy", "M/dd/yyyy", "d/MM/yyyy", "d/M/yyyy"]
-  },
-  {
-    data: "06-06-2021",
-    isValid: true,
-    result: ["dd-MM-yyyy", "MM-dd-yyyy"]
-  },
-  {
-    data: "06/12/98",
-    isValid: true,
-    result: ["dd/M/y", "dd/M/yy", "dd/MM/y", "dd/MM/yy", "MM/d/y", "MM/d/yy", "MM/dd/y", "MM/dd/yy"]
-  },
-  {
-    data: "6 01 1998",
-    isValid: true,
-    result: ["M dd yyyy", "d MM yyyy"]
-  },
-  {
-    data: "march 1 98",
-    isValid: true,
-    result: ["MMMM d yy", "MMMM d y"]
-  },
-  {
-    data: "06 sunday 1998",
-    isValid: true,
-    result: ["MM dddd yyyy"]
-  },
-  {
-    data: "31 Fri 2",
-    isValid: false,
-    result: ["y ddd M", "yy ddd M"]
-  },
-  {
-    data: "this is a test",
-    isValid: false,
-    result: []
-  },
-  {
-    data: "Friday 22 2021",
-    isValid: false,
-    result: []
-  }
-]
-
-let pythonTestCases = [
-  {
-    data: "2021-2-1",
-    isValid: true,
-    result: ["%Y-%-d-%-m", "%Y-%-m-%-d"]
-  },
-  {
-    data: "2021-02-01",
-    isValid: true,
-    result: ["%y-%d-%m", "%y-%m-%d"]
-  },
-  {
-    data: "feb 28 0006",
-    isValid: true,
-    result: ["MMM d yyyy", "MMM dd yyyy"]
-  },
-  {
-    data: "6/12/2021",
-    isValid: true,
-    result: ["M/d/yyyy", "M/dd/yyyy", "d/MM/yyyy", "d/M/yyyy"]
-  },
-  {
-    data: "06-06-2021",
-    isValid: true,
-    result: ["dd-MM-yyyy", "MM-dd-yyyy"]
-  },
-  {
-    data: "06/12/98",
-    isValid: true,
-    result: ["dd/M/y", "dd/M/yy", "dd/MM/y", "dd/MM/yy", "MM/d/y", "MM/d/yy", "MM/dd/y", "MM/dd/yy"]
-  },
-  {
-    data: "6 01 1998",
-    isValid: true,
-    result: ["M dd yyyy", "d MM yyyy"]
-  },
-  {
-    data: "march 1 98",
-    isValid: true,
-    result: ["MMMM d yy", "MMMM d y"]
-  },
-  {
-    data: "06 sunday 1998",
-    isValid: true,
-    result: ["MM dddd yyyy"]
-  },
-  {
-    data: "31 Fri 2",
-    isValid: false,
-    result: ["y ddd M", "yy ddd M"]
-  },
-  {
-    data: "this is a test",
-    isValid: false,
-    result: []
-  },
-  {
-    data: "Friday 22 2021",
-    isValid: false,
-    result: []
-  }
-]
 
 describe("General Parser Test", () => {
-  test('Invalid string returns empty list', () => {
-    // arrange 
-    var dateString = "sgsgsdgd";
+	test('Invalid string returns empty list', () => {
+		var dateString = "sgsgsdgd";
 
-    //and act
-    var result = parseDateString(dateString, "C#");
+		var result = parseDateString(dateString, "C#");
 
-    // assert
-    expect(result).toEqual([]);
-  });
+		expect(result).toEqual([]);
+	});
 })
 
 describe("C# Test Cases", () => {
-  //arrange
-  for (let sample of cSharpTestCases) {
-    test(`C# Test Case: ${sample.data}`, () => {
-      //act
-      var result = parseDateString(sample.data, "C#");
+	test("C# Test Case: 2021-2-1", () => {
+		var result = parseDateString("2021-2-1", "C#");
+		expect(new Set(result)).toEqual(new Set(['yyyy-d-M', 'yyyy-M-d']));
+	});
 
-      //assert
-      expect(new Set(result)).toEqual(new Set(sample.result));
-    });
-  }
+	test("C# Test Case: 2021-02-01", () => {
+		var result = parseDateString("2021-02-01", "C#");
+		expect(new Set(result)).toEqual(new Set(['yyyy-dd-MM', 'yyyy-MM-dd']));
+	});
+
+	test("C# Test Case: feb 28 0006", () => {
+		var result = parseDateString("feb 28 0006", "C#");
+		expect(new Set(result)).toEqual(new Set(['MMM d yyyy', 'MMM dd yyyy']));
+	});
+
+	test("C# Test Case: 6/12/2021", () => {
+		var result = parseDateString("6/12/2021", "C#");
+		expect(new Set(result)).toEqual(new Set(['M/d/yyyy', 'M/dd/yyyy', 'd/MM/yyyy', 'd/M/yyyy']));
+	});
+
+	test("C# Test Case: 06-06-2021", () => {
+		var result = parseDateString("06-06-2021", "C#");
+		expect(new Set(result)).toEqual(new Set(['dd-MM-yyyy', 'MM-dd-yyyy']));
+	});
+
+	test("C# Test Case: 06/12/98", () => {
+		var result = parseDateString("06/12/98", "C#");
+		expect(new Set(result)).toEqual(new Set(['dd/M/y', 'dd/M/yy', 'dd/MM/y', 'dd/MM/yy', 'MM/d/y', 'MM/d/yy', 'MM/dd/y', 'MM/dd/yy']));
+	});
+
+	test("C# Test Case: 6 01 1998", () => {
+		var result = parseDateString("6 01 1998", "C#");
+		expect(new Set(result)).toEqual(new Set(['M dd yyyy', 'd MM yyyy']));
+	});
+
+	test("C# Test Case: march 1 98", () => {
+		var result = parseDateString("march 1 98", "C#");
+		expect(new Set(result)).toEqual(new Set(['MMMM d yy', 'MMMM d y']));
+	});
+
+	test("C# Test Case: 06 sunday 1998", () => {
+		var result = parseDateString("06 sunday 1998", "C#");
+		expect(new Set(result)).toEqual(new Set(['MM dddd yyyy']));
+	});
+
+	test("C# Test Case: 31 Fri 2", () => {
+		var result = parseDateString("31 Fri 2", "C#");
+		expect(new Set(result)).toEqual(new Set(['y ddd M', 'yy ddd M']));
+	});
+
+	test("C# Test Case: this is a test", () => {
+		var result = parseDateString("this is a test", "C#");
+		expect(new Set(result)).toEqual(new Set([]));
+	});
+
+	test("C# Test Case: 2021-02-0", () => {
+		var result = parseDateString("2021-02-0", "C#");
+		expect(new Set(result)).toEqual(new Set([]));
+	});
+
 });
 
 describe("Python Test Cases", () => {
-  //arrange
-  for (let sample of pythonTestCases) {
-    test(`Python Test Case: ${sample.data}`, () => {
-      if(sample.data === "2021-2-1"){
-        var x = 5;
-      }
-      //act
-      var result = parseDateString(sample.data, "Python");
+	test("C# Test Case: 2021-2-1", () => {
+		var result = parseDateString("2021-2-1", "C#");
+		expect(new Set(result)).toEqual(new Set(['%Y-%-d-%-m', '%Y-%-m-%-d']));
+	});
 
-      //assert
-      expect(new Set(result)).toEqual(new Set(sample.result));
-    });
-  }
+	test("C# Test Case: 2021-02-01", () => {
+		var result = parseDateString("2021-02-01", "C#");
+		expect(new Set(result)).toEqual(new Set(['%y-%d-%m', '%y-%m-%d']));
+	});
+
+	test("C# Test Case: feb 28 0006", () => {
+		var result = parseDateString("feb 28 0006", "C#");
+		expect(new Set(result)).toEqual(new Set(['MMM d yyyy', 'MMM dd yyyy']));
+	});
+
+	test("C# Test Case: 6/12/2021", () => {
+		var result = parseDateString("6/12/2021", "C#");
+		expect(new Set(result)).toEqual(new Set(['M/d/yyyy', 'M/dd/yyyy', 'd/MM/yyyy', 'd/M/yyyy']));
+	});
+
+	test("C# Test Case: 06-06-2021", () => {
+		var result = parseDateString("06-06-2021", "C#");
+		expect(new Set(result)).toEqual(new Set(['dd-MM-yyyy', 'MM-dd-yyyy']));
+	});
+
+	test("C# Test Case: 06/12/98", () => {
+		var result = parseDateString("06/12/98", "C#");
+		expect(new Set(result)).toEqual(new Set(['dd/M/y', 'dd/M/yy', 'dd/MM/y', 'dd/MM/yy', 'MM/d/y', 'MM/d/yy', 'MM/dd/y', 'MM/dd/yy']));
+	});
+
+	test("C# Test Case: 6 01 1998", () => {
+		var result = parseDateString("6 01 1998", "C#");
+		expect(new Set(result)).toEqual(new Set(['M dd yyyy', 'd MM yyyy']));
+	});
+
+	test("C# Test Case: march 1 98", () => {
+		var result = parseDateString("march 1 98", "C#");
+		expect(new Set(result)).toEqual(new Set(['MMMM d yy', 'MMMM d y']));
+	});
+
+	test("C# Test Case: 06 sunday 1998", () => {
+		var result = parseDateString("06 sunday 1998", "C#");
+		expect(new Set(result)).toEqual(new Set(['MM dddd yyyy']));
+	});
+
+	test("C# Test Case: 31 Fri 2", () => {
+		var result = parseDateString("31 Fri 2", "C#");
+		expect(new Set(result)).toEqual(new Set(['y ddd M', 'yy ddd M']));
+	});
+
+	test("C# Test Case: this is a test", () => {
+		var result = parseDateString("this is a test", "C#");
+		expect(new Set(result)).toEqual(new Set([]));
+	});
+
+	test("C# Test Case: Friday 22 2021", () => {
+		var result = parseDateString("Friday 22 2021", "C#");
+		expect(new Set(result)).toEqual(new Set([]));
+	});
 });
